9d56df37eda86e13d39367e0d2fd9f491b1acd7b
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-10-03 13:02:10 -0500

    Define methods in class app.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
